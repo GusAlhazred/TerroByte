1c465e8e8f26f7f0354ba9e03b256099225a0855
Juan <juanfasalinas@gmail.com>
2022-08-30 00:18:05 -0300

    agregue el menu. Tiene un bug cuando se guarda

--- script/Loggin.js
+++ script/Loggin.js
@@ -1,24 +1,75 @@
-const menuUsuario = document.querySelector(".usuario");
-const listaMenuUsuario = ["Mi Perfil", "Cosas", "Mis posteos"]
+// const menuUsuario = document.querySelector(".usuario");
+// const menuUsuario = document.createElement("div");
+const menuUsuario = document.querySelector(".menuUsuario")
+
+// const main = document.querySelector("main")
+
+const triggerMenu = document.querySelector(".usuario")
+const listaMenuUsuario = [
+    {
+        nombre:"Mi Perfil",
+        link: "#" 
+    },
+    {
+        nombre:"Cosas", 
+        link: "#"
+    },
+    {
+        nombre:"Mis posteos",
+        link:"#"
+    }
+]
 
 let menuUsuarioDesplegado = false;
 
 
-const toogleMenu = () => {
-    if (!menuUsuarioDesplegado){
-        menuUsuario.classList.add("menuDesplegado");
-        const menu = document.createElement("ul")
+const generarImgParaMenu = () => {
+    const img = document.createElement("img");
+    img.createAttribute = "src";
+    img.src = "./img/user-pic-default.png";
+    img.classList.add("imagen-Menu");
+    img.addEventListener("click", toogleMenu)
+
+    console.log(img)
+
+    return(img)
+}
+
+const generarListaMenu = () => {
+    const menu = document.createElement("ul")
         for (punto of listaMenuUsuario){
             let elemento = document.createElement("li");
-            elemento.innerText=punto;
+            const hv = document.createElement("a");
+            hv.innerText=punto.nombre;
+            hv.createAttribute= "href";
+            hv.href=punto.link;
+            elemento.appendChild(hv)
             menu.appendChild(elemento);
         }
-        menuUsuario.appendChild(menu);
-        menuUsuarioDesplegado= true;
-    }
-    // menuUsuarioDesplegado? menuUsuarioDesplegado=false : menuUsuarioDesplegado=true ;
-    // menuUsuario.classList.toggle("menuDesplegado")
-    // console.log(menuUsuario.classList)
+        // menu.classList.add("escondido")
+        return menu
+}
+
+const generarMenu = () => {
+    menuUsuario.classList.add("menuDesplegado", "escondido")
+    menuUsuario.appendChild(generarImgParaMenu());
+    menuUsuario.appendChild(generarListaMenu());
+}
+const toogleMenu = () => {
+    menuUsuario.classList.toggle("escondido")
+    // if (menuUsuarioDesplegado){
+    //     menuUsuario.classList.toggle("escondido")
+    //     menuUsuario.remove();
+    //     menuUsuarioDesplegado = false;
+    // } else{
+    //     menuUsuarioDesplegado = true;
+    //     main.appendChild(generarMenu());
+
+    // }
 }   
 
-menuUsuario.addEventListener("click", toogleMenu)
+
+generarMenu();
+triggerMenu.addEventListener("click", toogleMenu)
+// console.log(main)
+// generarImgParaMenu()
